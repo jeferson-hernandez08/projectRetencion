3a876b4b206a0d49beb8f4ddb1a6a29c43c26194
Jeferson <jefer.hernandez1@gmail.com>
2025-03-18 16:35:58 -0500

    config global.php

---
+++ app/config/global.php
@@ -0,0 +1,12 @@
+<?php
+
+// Tiempo inactivo de 1 minuto | commit clase 13 prueba
+define("INACTIVE_TIME", 1);
+define("MAIN_APP_ROUTE", __DIR__.'/../app/');
+define("DRIVER", 'mysql');
+define("HOST", 'localhost');
+define("USERNAME_DB", 'root');
+define("PASSWORD_DB", '');
+define("DATABASE", 'gym_cpic');
+define("CHARSET", 'utf8mb4');
+define("COLLATION", 'utf8mb4_unicode_ci');
