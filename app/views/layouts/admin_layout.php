5c06bbee788f8ed8953faaa50b461ffde53dee2c
Jeferson <jefer.hernandez1@gmail.com>
2025-04-09 20:40:24 -0500

    Se agregan oconos correspondientes de cada tabla

--- app/views/layouts/admin_layout.php
+++ app/views/layouts/admin_layout.php
@@ -25,16 +25,16 @@
                             <li><a href="/centroFormacion/view"><i class="fas fa-building"></i><span class="info-text">Centros</span></a></li>
                             <li><a href="/programaFormacion/view"><i class="fas fa-dumbbell"></i><span class="info-text">Programas</span></a></li>
                         <?php endif ?>   
-                        <li><a href="/rol/view"><i class="fas fa-file-alt"></i><span class="info-text">Roles</span></a></li>
-                        <li><a href="/usuario/view"><i class="fas fa-user-cog"></i><span class="info-text">Usuarios</span></a></li>
-                        <li><a href="/programaFormacion/view"><i class="fas fa-bullseye"></i><span class="info-text">Programas</span></a></li>
+                        <li><a href="/rol/view"><i class="fas fa-users-cog"></i><span class="info-text">Roles</span></a></li>
+                        <li><a href="/usuario/view"><i class="fas fa-users"></i><span class="info-text">Usuarios</span></a></li>
+                        <li><a href="/programaFormacion/view"><i class="fas fa-chalkboard-teacher"></i><span class="info-text">Programas</span></a></li>
                         <li><a href="/grupo/view"><i class="fas fa-graduation-cap"></i><span class="info-text">Grupos</span></a></li>
-                        <li><a href="/aprendiz/view"><i class="fas fa-hands-helping"></i><span class="info-text">Aprendices</span></a></li>
-                        <li><a href="/reporte/view"><i class="fas fa-bullseye"></i><span class="info-text">Reportes</span></a></li>
-                        <li><a href="/estrategias/view"><i class="fas fa-bell"></i><span class="info-text">Estrategias</span></a></li>
-                        <li><a href="/categoria/view"><i class="fas fa-bell"></i><span class="info-text">Categorias</span></a></li>
-                        <li><a href="/causa/view"><i class="fas fa-exclamation-circle"></i><span class="info-text">Causas</span></a></li>
-                        <li><a href="/intervencion/view"><i class="fas fa-exclamation-circle"></i><span class="info-text">Intervenciones</span></a></li>
+                        <li><a href="/aprendiz/view"><i class="fas fa-user-graduate"></i><span class="info-text">Aprendices</span></a></li>
+                        <li><a href="/reporte/view"><i class="fas fa-chart-line"></i><span class="info-text">Reportes</span></a></li>
+                        <li><a href="/estrategias/view"><i class="fas fa-lightbulb"></i><span class="info-text">Estrategias</span></a></li>
+                        <li><a href="/categoria/view"><i class="fas fa-tags"></i><span class="info-text">Categorias</span></a></li>
+                        <li><a href="/causa/view"><i class="fas fa-question-circle"></i><span class="info-text">Causas</span></a></li>
+                        <li><a href="/intervencion/view"><i class="fas fa-hand-holding-heart"></i><span class="info-text">Intervenciones</span></a></li>
                         <?php if(isset($_SESSION['nombre'])) {  ?> 
                         <li><a href="/login/logout">
                                 <i class="fas fa-sign-in-alt"></i>
