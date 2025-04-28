05096f83fcf531e8781c510f5849c7d98d125d39
Jeferson <jefer.hernandez1@gmail.com>
2025-04-28 10:58:20 -0500

    Se agraga paginacion de bienvenida con maquetado y estilos (Jose Miguel Sierra)

--- app/views/main/welcome.php
+++ app/views/main/welcome.php
@@ -1,12 +1,19 @@
-<div class="data-container">
-    <div class="navegate-group">
-        <h2>Bienvenido a la Página Principal</h2>
-    </div>
-    <div class="info">
-        <div class="container-links">
-            <ul>
-                Hola Styles aqui
-            </ul>
+<link rel="stylesheet" href="/css/bienvenida.css">
+<div class="contenedor-principal">
+    <div class="contenido-caja">
+        <div class="contenido-texto">
+            <h1 class="titulo-principal">¡Bienvenido!</h1>
+            <h2 class="subtitulo">QUERIDO USUARIO</h2>
+            <p class="parrafo">
+                ¡Bienvenido a nuestra página de asistencias! Aquí encontrarás un espacio dedicado a brindarte el apoyo y la información que necesitas para resolver tus dudas y mejorar tu experiencia. Nuestro equipo está comprometido a ofrecerle respuestas rápidas y efectivas, asegurando que cada consulta sea atendida con la mayor atención y profesionalismo. No dudes en explorar nuestros recursos y ponerte en contacto con nosotros para cualquier pregunta o inquietud. ¡Estamos aquí para ayudarte!
+            </p>
         </div>
+
+        <!-- Elementos decorativos -->
+        <div class="circulo verde1"></div>
+        <div class="circulo verde2">
+            <img src="/img/logoSena.png" alt="Logo Sena" class="logo">
+        </div>
+        <div class="circulo verde3"></div>
     </div>
 </div>
