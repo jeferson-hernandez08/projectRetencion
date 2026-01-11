9f8cbcea291b77f0dd8762e8117347b538efb3cb
Jeferson <jefer.hernandez1@gmail.com>
2026-01-10 19:12:40 -0500

    Style: Se arregla botón de crear en aprendiz en responsive en viewAprendiz

--- app/views/aprendiz/viewAprendiz.php
+++ app/views/aprendiz/viewAprendiz.php
@@ -1,12 +1,12 @@
 <div class="data-container">
     <div class="navegate-group">
-        <div class="back">
+        <!-- <div class="back">
             <a href="/aprendiz/view"><img src="/img/back.svg"></a>
-        </div>
+        </div> -->
         <div class="create">
             <a href="/aprendiz/new">
             <button>
-                <i class="fa fa-plus-circle"></i> Crear aprendiz
+                <i class="fa fa-plus-circle"></i> <p>Crear aprendiz</p>
             </button>
             </a>
         </div>
@@ -180,7 +180,7 @@
     display: inline-flex;
     align-items: center;
     gap: 8px;
-    padding: 2px 20px;
+    padding: 5px 20px;
     background: #28a745;
     color: white;
     text-decoration: none;
@@ -405,7 +405,7 @@
 }
 
 i.fa-file-excel {
-    font-size: 23px;
+    /* font-size: 23px; */
     color: #FFFFFFFF; /* Blanco */
 }
 
